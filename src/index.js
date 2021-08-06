6461ee60c1f7781114a53f0d275d45be6b2df294
BurakOlga <Oljaburak@mail.ru>
2021-08-06 14:52:35 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -38,7 +38,32 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let text = '';
+    for (let i=0; i<expr.length;i+=10){
+      let unit; 
+      if (expr[i]==='1' || expr[i]==='0'){
+        unit = expr.substr(i,10);
+      } else{ 
+      unit = expr.substr(i,8);
+      }
+      
+      if (unit=='********'){
+        text +=' ';
+      } else{
+        let letter='';
+        for (let j=0; j<unit.length; j+=2){
+          let sumbol = unit.substr(j,2)
+          if (sumbol === '10'){
+            letter +='.';
+          }
+          if (sumbol ==='11'){
+            letter +='-';
+          }
+        }
+        text += MORSE_TABLE[letter]
+      }
+    }
+    return text;
 }
 
 module.exports = {
